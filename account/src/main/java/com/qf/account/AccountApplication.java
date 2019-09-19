392775891a15698affa75acf313237f1afc387f0
yansongzun <1006636887@qq.com>
2019-09-19 19:34:31 +0800

    初稿

---
+++ account/src/main/java/com/qf/account/AccountApplication.java
@@ -0,0 +1,13 @@
+package com.qf.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+}
